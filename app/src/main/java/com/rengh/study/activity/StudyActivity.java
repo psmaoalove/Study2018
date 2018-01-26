653ccf80268f0eaf29e6d1e66c15a5633a6d31a7
Guanghui Ren <renguanghui@le.com>
2018-01-26 09:05:14 +0800

    Study: Add proxy and exoplayer.
    Signed-off-by: Guanghui Ren <renguanghui@le.com>

--- app/src/main/java/com/rengh/study/activity/StudyActivity.java
+++ app/src/main/java/com/rengh/study/activity/StudyActivity.java
@@ -1,15 +1,46 @@
 package com.rengh.study.activity;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rengh.study.R;
 
 public class StudyActivity extends AppCompatActivity {
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
+
+        // 1. Create a default TrackSelector
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // 2. Create the player
+        SimpleExoPlayer player = ExoPlayerFactory
+                .newSimpleInstance(context, trackSelector);
+
+        // Bind the player to the view.
+        SimpleExoPlayerView simpleExoPlayerView = new SimpleExoPlayerView(context);
+        simpleExoPlayerView.setPlayer(player);
     }
+
+
 }
