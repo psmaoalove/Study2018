6586a7d2fac956f6a59ef9134e8a9dd606a9916e
Guanghui <renguanghui@le.com>
2018-02-08 14:03:45 +0800

    Study: Add leak canary.
    Signed-off-by: Guanghui <renguanghui@le.com>

--- app/src/main/java/com/rengh/study/application/StudyApplication.java
+++ app/src/main/java/com/rengh/study/application/StudyApplication.java
@@ -2,10 +2,19 @@ package com.rengh.study.application;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by rengh on 18-1-15.
  */
-
 public class StudyApplication extends Application {
     private static final String TAG = "StudyApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
+        }
+    }
 }
